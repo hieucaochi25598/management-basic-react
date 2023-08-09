6307e31269c20ae1eb481ea9f4d8b9b7118f037a
Hieu Cao <hieucaochi25598@gmail.com>
2023-08-10 00:08:25 +0700

    Install redux react-query

--- src/reducers/productSlice.ts
+++ src/reducers/productSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
-import { IProductSliceState } from '../types/model/product/ProductSliceStateModel';
+import { IProductSliceState } from '../types/model/product/IProductSliceStateModel';
 import { IProduct } from '../types/model/product/IProduct';
 
 const initialState: IProductSliceState = {
